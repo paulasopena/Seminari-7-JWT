3cec19bdcf35867da7741f416c672e12af640590
paulasopenacoello <paulasopenacoello@gmail.com>
2023-03-30 00:24:19 +0200

    first commit

---
+++ src/routes/subject.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from "express";
+import { delete_Subject,get_Subject,get_Subjects,matriculate_Subject,post_Subject, update_Subject } from "../controllers/subject";
+import { logMiddleware } from "../middleware/log";
+
+const router=Router();
+
+router.get("/all", logMiddleware, get_Subjects);
+router.get("/:idSubject", get_Subject);
+router.post("/", post_Subject);
+router.put("/:idSubject", update_Subject);
+router.delete("/:idSubject", delete_Subject);
+router.post("/matriculate", matriculate_Subject);
+
+export{router};
